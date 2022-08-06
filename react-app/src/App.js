c00600f1c77f9d377c60928f21e9d2320501ea80
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-08-06 14:07:25 -0700

    auto push

---
+++ react-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <h1>Hello Dojo!</h1>
+        <ul>
+                <li>Learn React!</li>
+                <li>CLimb Everest</li>
+                <li>Run a mountian</li>
+                <li>Feed the dogs</li>
+        </ul>
+    </>
+  );
+}
+
+export default App;
